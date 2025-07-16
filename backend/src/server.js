e86205239b688e2e7bfb01e4cc3c1912a430cf88
Henok <henokaragaw17@gmail.com>
2025-07-16 10:47:45 -0900

    minor server setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,24 @@
 import express from 'express'
+import cors from "cors"
+import {clerkMiddleware} from "@clerk/express"
+
+
+import userRouter from './routes/user.route.js';
+
 import { connectDB } from './config/db.js';
 import { ENV } from './config/env.js';
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
+app.use(clerkMiddleware());
 
 app.get("/", (req,res) => res.send('Hello from server'));
 
+app.use("/api/users", userRouter);
 
 const startServer = async () => {
     try {
