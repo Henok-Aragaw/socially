b7788dbde8c4123003bff844fee513075f624b17
Henok <henokaragaw17@gmail.com>
2025-07-16 10:40:18 -0900

    update middleware

--- backend/src/middleware/auth.middleware.js
+++ backend/src/middleware/auth.middleware.js
@@ -1,6 +1,11 @@
 export const protectRoute = async (req,res, next) => {
-    if(!req.auth().isAuthenticated){
-        return res.status(401).json({ message: "Unauthorized - you must be logged in" }); 
-    }
-    next();
-};
+       try {
+           const auth = req.auth();
+           if (!auth || !auth.isAuthenticated) {
+              return res.status(401).json({ message: "Unauthorized - you must be logged in" });
+           }
+       } catch (error) {
+             return res.status(401).json({ message: "Unauthorized - you must be logged in" }); 
+         }
+         next();
+     };
