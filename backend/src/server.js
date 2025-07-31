5f49b6cffaa2a97f65a5971a1b7458b5fdf102ea
Henok <henokaragaw17@gmail.com>
2025-07-31 02:40:05 -0900

    comment and notification api setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,6 +5,9 @@ import {clerkMiddleware} from "@clerk/express"
 
 import userRoutes from './routes/user.route.js';
 import postRoutes from './routes/post.route.js'
+import commentRoutes from "./routes/comment.route.js"
+import notificationRoutes from "./routes/notification.route.js"
+
 
 import { connectDB } from './config/db.js';
 import { ENV } from './config/env.js';
@@ -21,6 +24,14 @@ app.get("/", (req,res) => res.send('Hello from server'));
 
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/notifications", notificationRoutes)
+
+//error handling middleware
+app.use((err, req, res, next) => {
+  console.error("Unhandled error:", err);
+  res.status(500).json({ error: "Internal Server Error" });
+})
 
 const startServer = async () => {
     try {
