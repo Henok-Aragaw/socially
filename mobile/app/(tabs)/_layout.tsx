70edd3aad9f3a29c39988409a5fdc46af5660a81
Henok-Aragaw <henokaragaw17@gmail.com>
2025-09-03 18:37:16 +0300

    feat: api integration and profile ui development

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -34,6 +34,34 @@ const TabsLayout = () => {
             tabBarIcon: ({ color, size}) => <Feather name="home" size={size} color={color}/>
          }}
          />
+         <Tabs.Screen
+         name="search"
+         options={{
+            title:"",
+            tabBarIcon: ({ color, size}) => <Feather name="search" size={size} color={color}/>
+         }}
+         />
+         <Tabs.Screen
+         name="notifications"
+         options={{
+            title:"",
+            tabBarIcon: ({ color, size}) => <Feather name="bell" size={size} color={color}/>
+         }}
+         />
+         <Tabs.Screen
+         name="messages"
+         options={{
+            title:"",
+            tabBarIcon: ({ color, size}) => <Feather name="message-square" size={size} color={color}/>
+         }}
+         />
+         <Tabs.Screen
+         name="profile"
+         options={{
+            title:"",
+            tabBarIcon: ({ color, size}) => <Feather name="user" size={size} color={color}/>
+         }}
+         />
         </Tabs>
     )
 
